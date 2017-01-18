622eb1b9e39bca049300a19dfb8bc0f0d98b49f9
bouncepaw <tims102rus@gmail.com>
2017-01-18 16:10:53 +0500

    kekgen at its best
    hahaha

--- kekgen/kekgen.js
+++ kekgen/kekgen.js
@@ -1,11 +1,17 @@
 function doKek() {
-  $("#output").css("background", getRandomColor());
+  $("#output").css("background-color", getRandomColor());
   $("#kek").css({
     "color": getRandomColor(),
     "font-size": getRandomSizePx(),
     "font-family": getRandomFont()
-  })
+  });
 };
+function getCopy() {
+  $("#copy-desc").removeClass("shame");
+  $("#copy").removeClass("shame");
+  $("#copy").text(getKekAsHtml());
+};
+
 function getRandomNumber(max) {
   return Math.floor(Math.random() * max); // return a random number from 0 to max - 1, have no idea how it works
 };
@@ -24,3 +30,19 @@ function getRandomFont() {
       return "'PT Serif', serif";
   };
 };
+
+function getKekAsHtml() {
+  return '<style>' + getCssOfOutput() + getCssOfKek() + '</style>' + '<div id="output"><span id="kek">КЕК</span></div>';
+};
+function getCssOfOutput() {
+  var a = "#output{width:500px;height:300px;line-height:300px;border:1px black solid;border-radius:5px;display:inline-block;background-color:";
+  var b = $("#output").css("background");
+  return a + b + "}";
+};
+function getCssOfKek() {
+  var a = "#kek{display:inline-block;vertical-align:middle;color:"
+  var b = $("#kek").css("color");
+  var c = $("#kek").css("font-size");
+  var d = $("#kek").css("font-family");
+  return a + b + ";font-size:" + c + ";font-family:" + d + ";}";
+}
